67aef69bcea1a26d3a4292b9bc7f081028bf416e
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-09-09 20:07:35 +0430

    Calculating the difference between columns, adding new column, use the sort_value.

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
 import pandas as pd
 
 df = pd.read_csv('003 salaries-by-college-major.csv')
-# print 5 first rows.
 print(df.head())
 
-
-# print the all rows.
 print(df.shape)
 
-
-# to see the number of rows and columns.
 print(df.isna())
 
-# to print the 5 last rows.
 print(df.tail())
 
-
-# to delete the last row of cv.
 clean_df = df.dropna()
 print(clean_df.tail())
 
-# to  see all the values printed out below the cell for just this column
 print(clean_df['Starting Median Salary'])
 
-
-# to find the highest starting salary
 highest = clean_df['Starting Median Salary'].max()
 print(highest)
 
-# index for the row with the largest value
 largest_value = clean_df['Starting Median Salary'].idxmax()
 print(largest_value)
 
-# To see the name of the major that corresponds to that particular row, we can use the .loc (location) property.
 name_of_the_major = clean_df['Undergraduate Major'].loc[43]
 print(name_of_the_major)
 
-# If you don't specify a particular column you can use the .loc property to retrieve an entire row.
 print(clean_df.loc[43])
 
 # the highest mid-career salary:
@@ -52,3 +38,28 @@ print(clean_df['Undergraduate Major'].loc[clean_df['Starting Median Salary'].idx
 
 # the lowest mid-career salary and how much can people expect to earn with this degree:
 print(clean_df.loc[clean_df['Mid-Career Median Salary'].idxmax()])
+
+
+# calculate the difference between the earnings of the 10th and 90th percentile:
+print(clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary'])
+print(clean_df['Mid-Career 90th Percentile Salary'].subtract(clean_df['Mid-Career 10th Percentile Salary']))
+
+
+# adding the new column(spread) that is the result of the above calculation, with .insert():
+spread_col = clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
+clean_df.insert(1, 'Spread', spread_col)
+print(clean_df.head())
+
+
+low_risk = clean_df.sort_values('Spread')
+print(low_risk)
+
+print(low_risk[['Undergraduate Major', 'Spread']].head())
+
+# major with the highest_potential:
+highest_potential = clean_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
+print(highest_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head())
+
+# Majors with the Greatest Spread in Salaries
+highest_spread = clean_df.sort_values('Spread', ascending=False)
+print(highest_spread[['Undergraduate Major', 'Spread']].head())
